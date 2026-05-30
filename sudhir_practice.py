8f805d6de017f3356ffb3c1f2ccf417bc2652ac5
Sudhir Dhankar <dhankarsudhir07@gmail.com>
2026-05-30 16:26:08 +0530

    Add sudhir_practice.py - Python learning exercises

---
+++ sudhir_practice.py
@@ -0,0 +1,79 @@
+# age = int(input("please enter your age "))
+
+# if age < 13:
+#     print("you are a child")
+# elif age < 20:
+#     print ("you are a teenager ")   
+# elif age < 60 :
+#     print("you are an adult")
+# else:
+#     print("wassup unc")   
+
+# age = int(input("please enter your age"))
+# day = input (" what day is today ?")
+
+# if age > 18 :
+#     print ("ticket price is 12$")
+  
+# elif age < 18 and day == "wednesday" :
+#     print ("ticket price is 6$")
+# elif age < 18 :
+#     print("ticket price is 8$")       
+# else:
+#     print ("ticket price is 10$")     
+
+
+# password = input("please enter your password")
+# passwordcharlength = len(password)
+
+# if passwordcharlength < 6 : print("your password is weak ")
+# elif passwordcharlength <= 10  : print("your password is medium ")
+# else : print("your password is strong")
+
+
+# year = 2032
+# if year % 4 == 0  and year % 100 != 0:
+#     print ("this is a leap year") 
+
+# elif year % 400 == 0:
+#     print("this is a leap year")
+# else: print("this is not a leap year ")    
+
+# n = 10
+# sumofeven = 0
+
+# for nums in range (1, n+1 ):
+#     if nums % 2 == 0:
+#         sumofeven = sumofeven + nums 
+# print("sum of even numbers from 1 to ", n , "is", sumofeven)        
+
+# given_number = 10
+# sumofevennums = 0
+
+# for nums in range(0,given_number+1):
+#     if nums % 2 == 0:
+#      sumofevennums = sumofevennums + nums 
+# print(sumofevennums)    
+
+# num = 5
+
+# for multiplier in range(0,11):
+#     value = num * multiplier
+#     if multiplier == 5:
+#        pass 
+#     else: 
+#        print(num, "*", multiplier, "=", value)
+
+# name = 'sudhir'
+# reversedname = ''
+
+# for characters in name:
+#   reversedname = characters + reversedname 
+
+# print(reversedname)  
+
+# text = 'teeter'
+
+# for char in text:
+#     if text.count(char) == 1:
+#         print(char)
